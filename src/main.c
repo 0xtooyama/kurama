cfb0016255ddd75b6af4af8be65546dc6372f64c
0xtooyama <45735463+0xtooyama@users.noreply.github.com>
2019-07-10 03:05:04 +0900

    ADD short option

--- src/main.c
+++ src/main.c
@@ -7,6 +7,7 @@
 #include <dirent.h>
 #include <unistd.h>
 #include <time.h>
+#include <getopt.h>
 
 // tags
 #define A_TAG   1
@@ -16,17 +17,28 @@
 #define HREF_ATTR 1
 #define SRC_ATTR  2
 
+struct option long_options[] = {
+    {"url", required_argument, NULL, (int)'u'},
+    {"savedir", required_argument, NULL, (int)'s'},
+    {"fmt", required_argument, NULL, (int)'f'},
+    {"mainpage", required_argument, NULL, (int)'m'},
+    {"logfile", required_argument, NULL, (int)'l'},
+    {"tag", required_argument, NULL, (int)'t'},
+    {"seq_name", no_argument, NULL, (int)'1'}
+};
+
 typedef struct {
     char *url;
     int urllen;
-    char *mainpage_name;
     char *savedir;
     char *targetfmt;
+    char *mainpage_name;
     char *wget_logfile;
-    char *links_file;
     int targettag;
-    int sequeantial_save;
-} args_t;
+    int sequential_save;
+
+    char *links_file;
+} opts_t;
 
 typedef struct atag_t {
     char *href;
@@ -48,173 +60,142 @@ typedef struct link_t {
     struct link_t *next;
 } link_t;
 
-args_t * parse_args(int argc, char **argv) {
-    args_t *args = (args_t *)calloc(1, sizeof(args_t));
-    if (!args) {
-        fprintf(stderr, "args calloc error\n");
+opts_t * parse_opts(int argc, char **argv) {
+    opts_t *opts = (opts_t *)calloc(1, sizeof(opts_t));
+    if (!opts) {
+        fprintf(stderr, "opts calloc error\n");
         exit(1);
     }
 
-    for (int i = 1; i < argc; i++) {
-        char *word = argv[i];
-        if (word[0] == '-') {
-            char opt[16];
-            strcpy(opt, &word[1]);
-            if (!strcmp(opt, "url")) {
-                args->url = (char *)calloc(strlen(argv[i + 1]) + 1, sizeof(char));
-                if (!args->url) {
-                    fprintf(stderr, "args->url calloc error\n");
-                    //free_args();
-                    exit(1);
-                }
-                strcpy(args->url, argv[i + 1]);
-                args->urllen = strlen(args->url);
-                i++;
-                continue;
-            } else if (!strcmp(opt, "savedir")) {
-                args->savedir = (char *)calloc(strlen(argv[i + 1]) + 1, sizeof(char));
-                if (!args->savedir) {
-                    fprintf(stderr, "args->savedir calloc error\n");
-                    //free_args();
-                    exit(1);
-                }
-                strcpy(args->savedir, argv[i + 1]);
-                i++;
-                continue;
-            } else if (!strcmp(opt, "targetfmt")) {
-                args->targetfmt = (char *)calloc(strlen(argv[i + 1]) + 1, sizeof(char));
-                if (!args->targetfmt) {
-                    fprintf(stderr, "args->targetfmt calloc error\n");
-                    //free_args();
-                    exit(1);
-                }
-                strcpy(args->targetfmt, argv[i + 1]);
-                i++;
-                continue;
-            } else if (!strcmp(opt, "mainpage_name")) {
-                args->mainpage_name = (char *)calloc(strlen(argv[i + 1]) + 1, sizeof(char));
-                if (!args->mainpage_name) {
-                    fprintf(stderr, "args->targetfmt calloc error\n");
-                    //free_args();
-                    exit(1);
-                }
-                strcpy(args->mainpage_name, argv[i + 1]);
-                i++;
-                continue;
-            } else if (!strcmp(opt, "wget_logfile")) {
-                args->wget_logfile = (char *)calloc(strlen(argv[i + 1]) + 1, sizeof(char));
-                if (!args->wget_logfile) {
-                    fprintf(stderr, "args->wget_logfile calloc error\n");
-                    //free_args();
-                    exit(1);
-                }
-                strcpy(args->wget_logfile, argv[i + 1]);
-                i++;
-                continue;
-            } else if (!strcmp(opt, "targettag")) {
-                char *targettag = (char *)calloc(strlen(argv[i + 1]) + 1, sizeof(char));
-                if (!targettag) {
-                    fprintf(stderr, "targettag calloc error\n");
-                    //free_args();
-                    exit(1);
-                }
-                strcpy(targettag, argv[i + 1]);
-                if (!strcmp(targettag, "a")) {
-                    args->targettag = A_TAG;
-                } else if (!strcmp(targettag, "img")) {
-                    args->targettag = IMG_TAG;
-                }
-                i++;
-                continue;
-            } else if (!strcmp(opt, "sequentialsave")) {
-                args->sequeantial_save = 1;
-                continue;
-            } else {
-                fprintf(stderr, "option '%s' is not decrared\n", opt);
-                //free_args();
-                exit(1);
-            }
-        } else {
-            fprintf(stderr, "need options to execute\n");
-            //free_args();
+    int opt = 0;
+    while ((opt = getopt_long(argc, argv, "u:s:f:m:l:t:", long_options, NULL)) != -1) {
+        if (opt == '1') { // sequential_save
+            opts->sequential_save = 1;
+            continue;
+        }
+
+        char *opt_value = (char *)calloc(strlen(optarg) + 1, sizeof(char));
+        if (!opt_value) {
+            fprintf(stderr, "opt_value calloc error\n");
+            //free_opts();
             exit(1);
         }
+
+        strcpy(opt_value, optarg);
+
+        switch (opt) {
+            case 'u': 
+                opts->url = opt_value;
+                opts->urllen = strlen(opt_value);
+                break;
+            case 's':
+                opts->savedir = opt_value;
+                break;
+            case 'f':
+                opts->targetfmt = opt_value;
+                break;
+            case 'm':
+                opts->mainpage_name = opt_value;
+                break;
+            case 'l':
+                opts->wget_logfile = opt_value;
+                break;
+            case 't':
+                if (!strcmp(opt_value, "a")) opts->targettag = A_TAG;
+                else if (!strcmp(opt_value, "img")) opts->targettag = IMG_TAG;
+                break;
+            default :
+                // free_opts(opts);
+                exit(1);
+        }
     }
-    return args;
+
+    if (argc > optind && opts->urllen == 0) {
+        opts->url = (char *)calloc(strlen(argv[optind]) + 1, sizeof(char));
+        if (!opts->url) {
+            fprintf(stderr, "opts->url calloc error\n");
+            //free_opts();
+            exit(1);
+        }
+        strcpy(opts->url, argv[optind]);
+        opts->urllen = strlen(argv[optind]);
+    }
+
+    return opts;
 }
 
 char * get_now() {
     time_t t = time(NULL);
     if (t == -1) {
         fprintf(stderr, "get time erorr\n");
-        // free_args()
+        // free_opts()
         exit(1);
     }
     struct tm *tm = localtime(&t);
     if (tm == NULL) {
         fprintf(stderr, "get localtime erorr\n");
-        // free_args()
+        // free_opts()
         exit(1);
     }
     char *now = (char *)calloc(strlen("yyyyMMddhhmmss") + 1, sizeof (char));
     if (!now) {
         fprintf(stderr, "now calloc error\n");
-        //free_args();
+        //free_opts();
         exit(1);
     }
     sprintf(now, "%04d%02d%02d%02d%02d%02d", tm->tm_year + 1900, tm->tm_mon + 1, tm->tm_mday + 1, tm->tm_hour, tm->tm_min, tm->tm_sec);
     return now;
 }
 
-void default_args(args_t *args) {
-    if (args->savedir == NULL) {
-        args->savedir = get_now();
+void default_opts(opts_t *opts) {
+    if (opts->savedir == NULL) {
+        opts->savedir = get_now();
     }
 
-    if (args->mainpage_name == NULL) {
+    if (opts->mainpage_name == NULL) {
         char *mainpage_name = "main.html";
-        args->mainpage_name = (char *)calloc(strlen(mainpage_name) + 1, sizeof (char));
-        if (!args->mainpage_name) {
-            fprintf(stderr, "args->mainpage_name calloc error\n");
-            //free_args();
+        opts->mainpage_name = (char *)calloc(strlen(mainpage_name) + 1, sizeof (char));
+        if (!opts->mainpage_name) {
+            fprintf(stderr, "opts->mainpage_name calloc error\n");
+            //free_opts();
             exit(1);
         }
-        strcpy(args->mainpage_name, mainpage_name);
+        strcpy(opts->mainpage_name, mainpage_name);
     }
 
-    if (args->wget_logfile == NULL) {
+    if (opts->wget_logfile == NULL) {
         char *wget_logfile = "wget_log.txt";
-        args->wget_logfile = (char *)calloc(strlen(wget_logfile) + 1, sizeof (char));
-        if (!args->wget_logfile) {
-            fprintf(stderr, "args->wget_logfile calloc error\n");
-            //free_args();
+        opts->wget_logfile = (char *)calloc(strlen(wget_logfile) + 1, sizeof (char));
+        if (!opts->wget_logfile) {
+            fprintf(stderr, "opts->wget_logfile calloc error\n");
+            //free_opts();
             exit(1);
         }
-        strcpy(args->wget_logfile, wget_logfile);
+        strcpy(opts->wget_logfile, wget_logfile);
     }
 
-    if (args->links_file == NULL) {
+    if (opts->links_file == NULL) {
         char *links_file = "links.txt";
-        args->links_file = (char *)calloc(strlen(links_file) + 1, sizeof (char));
-        if (!args->links_file) {
-            fprintf(stderr, "args->links_file calloc error\n");
-            //free_args();
+        opts->links_file = (char *)calloc(strlen(links_file) + 1, sizeof (char));
+        if (!opts->links_file) {
+            fprintf(stderr, "opts->links_file calloc error\n");
+            //free_opts();
             exit(1);
         }
-        strcpy(args->links_file, links_file);
+        strcpy(opts->links_file, links_file);
     }
 
-    if (args->targettag == 0) args->targettag = A_TAG;
-    if (args->sequeantial_save == 0) args->sequeantial_save = 0;
+    if (opts->targettag == 0) opts->targettag = A_TAG;
+    if (opts->sequential_save == 0) opts->sequential_save = 0;
 }
 
-void mk_savedir(args_t *args) {
-    char *savedir = args->savedir;
+void mk_savedir(opts_t *opts) {
+    char *savedir = opts->savedir;
     DIR *savedirp = opendir(savedir);
     if (savedirp) {
         fprintf(stderr, "directory %s is existed.\n", savedir);
         closedir(savedirp);
-        //free_args();
+        //free_opts();
         exit(1);
     }
 
@@ -225,24 +206,24 @@ void mk_savedir(args_t *args) {
     }
 }
 
-void cd_savedir(args_t *args) {
-    int rc = chdir(args->savedir);
+void cd_savedir(opts_t *opts) {
+    int rc = chdir(opts->savedir);
     if (rc == -1) {
-        fprintf(stderr, "chdir %s failed.\n", args->savedir);
-        //free_args()
+        fprintf(stderr, "chdir %s failed.\n", opts->savedir);
+        //free_opts()
         exit(1);
     }
 }
 
-void wget_mainpage(args_t *args) {
+void wget_mainpage(opts_t *opts) {
     char wgetcmd[2048];
-    sprintf(wgetcmd, "wget -o %s -O %s %s", args->wget_logfile, args->mainpage_name, args->url);
+    sprintf(wgetcmd, "wget -o %s -O %s %s", opts->wget_logfile, opts->mainpage_name, opts->url);
     printf("[wget command] %s\n", wgetcmd);
     system(wgetcmd);
-    FILE *mainpagep = fopen(args->mainpage_name, "r");
+    FILE *mainpagep = fopen(opts->mainpage_name, "r");
     if (!mainpagep) {
         fprintf(stderr, "wget mainpage failed.\n");
-        //free_args()
+        //free_opts()
         exit(1);
     }
     fclose(mainpagep);
@@ -278,7 +259,7 @@ char * get_tagattr(FILE *mainpagep, char *attr) {
     char *link = (char *)calloc(link_len + 1, sizeof (char));
     if (!link) {
         fprintf(stderr, "link calloc error\n");
-        //free_args();
+        //free_opts();
         exit(1);
     }
 
@@ -343,18 +324,18 @@ void get_token(FILE *mainpagep, char *token, int token_size) {
     }
 }
 
-mainpage_t * parse_mainpage(args_t *args) {
-    FILE *mainpagep = fopen(args->mainpage_name, "r");
+mainpage_t * parse_mainpage(opts_t *opts) {
+    FILE *mainpagep = fopen(opts->mainpage_name, "r");
     if (!mainpagep) {
         fprintf(stderr, "open mainpage failed.\n");
-        //free_args()
+        //free_opts()
         exit(1);
     }
 
     mainpage_t *mainpage = (mainpage_t *)calloc(1, sizeof (mainpage_t));
     if (!mainpage) {
         fprintf(stderr, "mainpage calloc error\n");
-        // free_args();
+        // free_opts();
         exit(1);
     }
     char c;
@@ -378,16 +359,16 @@ mainpage_t * parse_mainpage(args_t *args) {
     return mainpage;
 }
 
-link_t * get_atag_links(args_t *args, mainpage_t *mainpage) {
+link_t * get_atag_links(opts_t *opts, mainpage_t *mainpage) {
     link_t *links = NULL;
     link_t *ite = NULL;
     atag_t *atag = mainpage->atags;
     while (atag) {
-        if (strstr(atag->href, args->targetfmt)) {
+        if (strstr(atag->href, opts->targetfmt)) {
             link_t *link = (link_t *)calloc(1, sizeof (link_t));
             if (!link) {
                 fprintf(stderr, "links calloc error\n");
-                // free_args()
+                // free_opts()
                 exit(1);
             }
             link->value = atag->href;
@@ -406,16 +387,16 @@ link_t * get_atag_links(args_t *args, mainpage_t *mainpage) {
     return links;
 }
 
-link_t * get_imgtag_links(args_t *args, mainpage_t *mainpage) {
+link_t * get_imgtag_links(opts_t *opts, mainpage_t *mainpage) {
     link_t *links = NULL;
     link_t *ite = NULL;
     imgtag_t *imgtag = mainpage->imgtags;
     while (imgtag) {
-        if (strstr(imgtag->src, args->targetfmt)) {
+        if (strstr(imgtag->src, opts->targetfmt)) {
             link_t *link = (link_t *)calloc(1, sizeof (link_t));
             if (!link) {
                 fprintf(stderr, "links calloc error\n");
-                // free_args()
+                // free_opts()
                 exit(1);
             }
             link->value = imgtag->src;
@@ -434,19 +415,19 @@ link_t * get_imgtag_links(args_t *args, mainpage_t *mainpage) {
     return links;
 }
 
-link_t * get_tag_links(args_t *args, mainpage_t *mainpage) {
-    switch (args->targettag) {
-        case A_TAG:   return get_atag_links(args, mainpage);
-        case IMG_TAG: return get_imgtag_links(args, mainpage);
+link_t * get_tag_links(opts_t *opts, mainpage_t *mainpage) {
+    switch (opts->targettag) {
+        case A_TAG:   return get_atag_links(opts, mainpage);
+        case IMG_TAG: return get_imgtag_links(opts, mainpage);
     }
     return NULL;
 }
 
-void output_links(args_t *args, link_t *links) {
-    FILE *linksp = fopen(args->links_file, "w+");
+void output_links(opts_t *opts, link_t *links) {
+    FILE *linksp = fopen(opts->links_file, "w+");
     if (!linksp) {
         fprintf(stderr, "open linksp failed.\n");
-        //free_args()
+        //free_opts()
         exit(1);
     }
     link_t *ite = links;
@@ -479,12 +460,12 @@ void wget_confilm(link_t *links) {
     }
 }
 
-void wget_linksfile(args_t *args, link_t *links) {
-    output_links(args, links);
+void wget_linksfile(opts_t *opts, link_t *links) {
+    output_links(opts, links);
     wget_confilm(links);
     char wgetcmd[2048];
-    if (!args->sequeantial_save) {
-        sprintf(wgetcmd, "wget -a %s -i %s", args->wget_logfile, args->links_file);
+    if (!opts->sequential_save) {
+        sprintf(wgetcmd, "wget -a %s -i %s", opts->wget_logfile, opts->links_file);
         printf("[wget command] %s\n", wgetcmd);
         system(wgetcmd);
         return;
@@ -493,7 +474,7 @@ void wget_linksfile(args_t *args, link_t *links) {
     link_t *ite = links;
     int links_cnt = 0;
     while (ite) {
-        sprintf(wgetcmd, "wget -a %s -O %03d %s", args->wget_logfile, links_cnt, ite->value);
+        sprintf(wgetcmd, "wget -a %s -O %03d %s", opts->wget_logfile, links_cnt, ite->value);
         printf("[wget command] %s\n", wgetcmd);
         system(wgetcmd);
         ite = ite->next;
@@ -502,12 +483,12 @@ void wget_linksfile(args_t *args, link_t *links) {
 }
 
 int main(int argc, char **argv) {
-    args_t *args = parse_args(argc, argv);
-    default_args(args);
-    mk_savedir(args);
-    cd_savedir(args);
-    wget_mainpage(args);
-    mainpage_t *mainpage = parse_mainpage(args);
-    link_t *links = get_tag_links(args, mainpage);
-    wget_linksfile(args, links);
+    opts_t *opts = parse_opts(argc, argv);
+    default_opts(opts);
+    mk_savedir(opts);
+    cd_savedir(opts);
+    wget_mainpage(opts);
+    mainpage_t *mainpage = parse_mainpage(opts);
+    link_t *links = get_tag_links(opts, mainpage);
+    wget_linksfile(opts, links);
 }
